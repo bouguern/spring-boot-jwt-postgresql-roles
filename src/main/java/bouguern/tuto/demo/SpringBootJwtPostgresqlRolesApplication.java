02e21f84d862113b557e9272e9f95b548cc4051e
Bouguern Mohamed <bouguern.mohamed.10@gmail.com>
2026-02-23 22:28:52 +0000

    Project initiation

---
+++ src/main/java/bouguern/tuto/demo/SpringBootJwtPostgresqlRolesApplication.java
@@ -0,0 +1,13 @@
+package bouguern.tuto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtPostgresqlRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtPostgresqlRolesApplication.class, args);
+	}
+
+}
